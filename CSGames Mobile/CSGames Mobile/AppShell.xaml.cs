d3e09e29bf9166cb4302b0bb1b3aaf3b8b074608
Kamel <kamel.laurentd@gmail.com>
2023-03-17 19:52:13 -0400

    added classes and loadingPage

--- CSGames Mobile/CSGames Mobile/AppShell.xaml.cs
+++ CSGames Mobile/CSGames Mobile/AppShell.xaml.cs
@@ -5,6 +5,8 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
+		Routing.RegisterRoute("loadingpage", typeof(LoadingPage));
+		Routing.RegisterRoute("mainpage", typeof(MainPage));
 	}
 }
 
---
+++ CSGames Mobile/CSGames Mobile/classes/News.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CSGames_Mobile.classes;
+
+	public struct News
+	{
+	int id;
+	string message;
+	string title;
+	int type;
+	}
+
+
