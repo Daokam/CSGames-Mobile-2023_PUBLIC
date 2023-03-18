61a4a2549231bd4ebc43103c907082b02677b72a
Dominic Hains <contact.dominichains@gmail.com>
2023-03-17 21:03:23 -0400

    api fini

---
+++ CSGames Mobile/CSGames Mobile/Services/Power.cs
@@ -0,0 +1,16 @@
+using System;
+namespace CSGames_Mobile.Services
+{
+	public class Power
+	{
+		private int power_levels;
+
+		public Power(int power_levels)
+		{
+			this.power_levels = power_levels;
+		}
+
+		public int Power_levels => power_levels;
+	}
+}
+
---
+++ CSGames Mobile/CSGames Mobile/Services/TransitItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CSGames_Mobile.Services
+{
+    public class TransitItem
+    {
+        public int Id { get; set; }
+        public int Frequency { get; set; }
+        public int Line { get; set; }
+        public string Description { get; set; }
+        public string Schedule { get; set; }
+
+        public TransitItem(int id, int frequency, int line, string description, string schedule)
+        {
+            Id = id;
+            Frequency = frequency;
+            Line = line;
+            Description = description;
+            Schedule = schedule;
+        }
+    }
+}
+
